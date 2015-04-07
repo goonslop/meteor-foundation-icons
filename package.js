29330a0b18be28e052f051b708a6e4f3f81d4556
Kelley Maibaum <kelleytaylormaibaum@gmail.com>
2015-04-07 03:20:37 -0400

    initial commit

---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: 'maibaum:foundation-icons',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/maibaum/meteor-foundation-icons',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles([
+  	'foundation-icons.js',
+  	'foundation-icons/foundation-icons.eot',
+    'foundation-icons/foundation-icons.svg',
+    'foundation-icons/foundation-icons.ttf',
+    'foundation-icons/foundation-icons.woff',
+    'foundation-icons.css'
+    ]
+    , 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('maibaum:foundation-icons');
+});
